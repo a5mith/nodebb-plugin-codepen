ee80f5e1c3382a4e1622356d79534e0548e92693
a5mith <aaronsmith91@sky.com>
2014-10-02 19:12:34 +0100

    0.1.0~2

--- library.js
+++ library.js
@@ -2,11 +2,11 @@
     "use strict";
 
     var codepen = {},
-        embed = '<p data-height="268" data-theme-id="0" data-slug-hash="$2" data-default-tab="result" data-user="$1" class="codepen"></p><script async src="//codepen.io/assets/embed/ei.js"></script>';
+        embed = '<p data-height="268" data-theme-id="0" data-slug-hash="$2" data-default-tab="result" data-user="$1"></p><script async src="//codepen.io/assets/embed/ei.js"></script>';
 
 
     codepen.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(codepen\.)io\/.+\/pen\/([\w\-_]+)">.+<\/a>/g, embed);
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(codepen\.)io\/([\w\-_]+)\/pen\/([\w\-_]+)">([\w\-_]+)<\/a>/g, embed);
         callback(null, postContent);
     };
 
