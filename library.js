052c2a839210facd868e9fb98e9336cfdb43b5b6
a5mith <aaronsmith91@sky.com>
2014-10-02 19:01:24 +0100

    0.1.0
    Update for Codepens changed embed code, should now support direct URL
    placement.

--- library.js
+++ library.js
@@ -2,11 +2,11 @@
     "use strict";
 
     var codepen = {},
-        embed = '<p data-height="268" data-theme-id="0" data-slug-hash="$1" data-default-tab="result"></p><script async src="//codepen.io/assets/embed/ei.js"></script>';
+        embed = '<p data-height="268" data-theme-id="0" data-slug-hash="$2" data-default-tab="result"></p><script async src="//codepen.io/assets/embed/ei.js"></script>';
 
 
     codepen.parse = function(postContent, callback) {
-        postContent = postContent.replace(/\[codepen_embed slug_hash=([\s\S]*?)&[\s\S]*?\]/g, embed);
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(codepen\.)io\/.+\/pen\/([\w\-_]+)">.+<\/a>/g, embed);
         callback(null, postContent);
     };
 
