730e149213b9478fe38a772d5897897b851a52f8
a5mith <aaronsmith91@sky.com>
2014-11-18 23:35:40 +0000

    0.6.0 compatability

--- library.js
+++ library.js
@@ -3,11 +3,17 @@
 
     var codepen = {},
         embed = '<p data-height="268" data-theme-id="0" data-slug-hash="$2" data-default-tab="result" data-user="$1" class=\'codepen\'></p><script async src="//codepen.io/assets/embed/ei.js"></script>';
+    var pen = /<a href="(?:http?:\/\/)?(codepen)\.io\/.+\/pen\/([\w\-_]+)">.+<\/a>/g;
 
+    codepen.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(pen)) {
+            data.postData.content = data.postData.content.replace(pen, embed);
+        }
 
-    codepen.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(codepen)\.io\/.+\/pen\/([\w\-_]+)">.+<\/a>/g, embed);
-        callback(null, postContent);
+        callback(null, data);
     };
 
     module.exports = codepen;
